b278c7566319dea271865431d2cf4edf23c2e94e
kunzilex <zikish@ukr.net>
2022-07-02 14:12:09 -0400

    new lessons are added

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
 @pytest.fixture
 def get_chrome_options():
     options = chrome_options
